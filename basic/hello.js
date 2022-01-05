d3210bb10b380aeec1e4dd5f822edc5b603b89a2
Eutene Lin <eutenelin@gmail.com>
2022-01-05 18:19:08 +0800

    修改hello.js

--- basic/hello.js
+++ basic/hello.js
@@ -1,1 +1,9 @@
-console.log("Hello Node");
+let name = "年";
+
+console.log(`Hello Node from ${name}`);
+
+console.log("start");
+setTimeout(function () {
+  console.log("Timeout");
+}, 1000);
+console.log("end");
